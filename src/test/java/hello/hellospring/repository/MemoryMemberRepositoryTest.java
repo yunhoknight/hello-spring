06a97926eb020a9d7e3e5a863f3639532f6101f6
uknow <yunhoknight@naver.com>
2023-02-17 06:18:01 +0900

    14강 회원 서비스 테스트

--- src/test/java/hello/hellospring/repository/MemoryMemberRepositoryTest.java
+++ src/test/java/hello/hellospring/repository/MemoryMemberRepositoryTest.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
 
-class repository {
+class MemoryMemberRepositoryTest {
     MemoryMemberRepository repository = new MemoryMemberRepository();
 
     @AfterEach
